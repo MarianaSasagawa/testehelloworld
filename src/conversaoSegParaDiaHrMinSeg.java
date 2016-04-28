d174a58540b2e1717172bea798a22e6e7d71889f
MarianaSasagawa <mariana.sasagawa@hotmail.com>
2016-04-28 17:16:25 -0300

    add_comentarios

--- src/conversaoSegParaDiaHrMinSeg.java
+++ src/conversaoSegParaDiaHrMinSeg.java
@@ -7,7 +7,7 @@ public class conversaoSegParaDiaHrMinSeg {
 	 * @param args
 	 */
 	
-	private static Scanner scan;
+	private static Scanner scan;	// lê o que for digitado
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -20,18 +20,18 @@ public class conversaoSegParaDiaHrMinSeg {
 		
 		N = scan.nextInt();
 		
-		while (N > 86400) {
+		while (N > 86400) {		// 1 dia possui 86400 segundos
 			dia++;
 			N = N - 86400;
 			
 		}
 		
-		while (N >= 3600) {
+		while (N >= 3600) {		// 1 hora possui 3600 segundos
 			N = N - 3600;
 			hora++;
 		}
 		
-		while (N >= 60) {
+		while (N >= 60) {		// 1 minuto possui 60 segundos
 			N = N - 60;
 			min++;
 		}
